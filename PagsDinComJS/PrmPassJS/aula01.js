636ee6eaab5fdd29f2d46a8e4551cc8db47145c8
Aster(NoemyT) <noemy_torres9@hotmail.com>
2023-10-25 23:03:59 -0300

    Trilha JS

---
+++ PagsDinComJS/PrmPassJS/aula01.js
@@ -0,0 +1,14 @@
+// Aula01 - Definindo 
+
+let v1 = 15; // Definir variáveis
+const pi = 3.14; // Definir constantes
+
+console.log(v1); // Função para imprimir/print
+console.log(pi);
+
+let v2 = 5;
+
+console.log(v1 + v2); // soma
+console.log(v1 - v2); // subtração
+console.log(v1 * v2); // multiplicação
+console.log(v1 / v2); // divisão
